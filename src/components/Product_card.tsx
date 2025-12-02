1f57a96fbcbba56184eec59a66aefd928ccd8baa
Milkesa Dereje <69218039+Mikydad@users.noreply.github.com>
2025-12-02 07:36:50 +0300

    added a stars feature

--- src/components/Product_card.tsx
+++ src/components/Product_card.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Star } from "lucide-react";
 
-function Product_card() {
+type ProductCardProps = {
+  src: string;
+  alt?: string;
+  title: string;
+  rating: number; // ⭐⭐⭐⭐☆
+  amount: string; // price
+};
+
+const Stars = ({ value }: { value: number }) => {
   return (
-<div className="main bg-amber-600 h-60 w-45 rounded-xs">
-    
+    <div className="flex gap-1">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <Star
+          key={i}
+          size={18}
+          className={
+            i < value
+              ? "text-yellow-400 fill-yellow-400"
+              : "text-gray-300"
+          }
+        />
+      ))}
+    </div>
+  );
+};
+
+
+function Product_card({ src, alt, title, rating, amount }: ProductCardProps) {
+  return (
+<div className="main">
+    <div className="image_section bg-amber-600 h-60 w-50 rounded-xl">
+      <img src={src} alt={alt} />
+    </div>
+    <div className="headers">
+      <h2>{title}</h2>
+        <Stars value={rating} />
+      <h3>Amount</h3>
+    </div>
 </div>  
 )
 }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Product_card from './Product_card'
-function Products() {
+function Products(props) {
   return (
 <div className="main">
     <div className="header flex items-center justify-center p-3 font-semibold">
         <h1 className=''>Find From Our Best Products</h1>
     </div>
     <div className="cards_section flex justify-center p-5">
-        <Product_card />
+  
+  <Product_card src='./public/t-shirt.webp'alt="Cool T-Shirt"
+  title="Casual Oversized Tee"
+  rating={4}
+  amount="$29.99"/>
     </div>
 </div>  
 )
